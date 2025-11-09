dedebe8c7c171d5a7ab237d83c0d4318c1976915
Jakub Graczyk <j.graczyk4@student.uw.edu.pl>
2025-11-09 21:12:08 +0100

    add: async python and rust-py03 functions + benchmarks

--- python/main.py
+++ python/main.py
@@ -1,14 +1,100 @@
-from lib import sum_as_string_python
-from py03_benchmark.py03_benchmark import sum_as_string
+import asyncio
+import os
+import time
+from pathlib import Path
+
+import plotly.express as px
+import plotly.graph_objects as go
+from lib import (
+    async_collection_add_python,
+    async_collection_tokio_add_python,
+    sum_as_string_python,
+)
+from py03_benchmark.py03_benchmark import (
+    async_collection_add,
+    async_collection_tokio_add,
+    sum_as_string,
+)
 
 
-def main():
-    print("Hello from py03-benchmark!")
-    result = sum_as_string(1, 2)
-    print(result)
-    result = sum_as_string_python(1, 2)
-    print(result)
+async def async_rust(calls: int, size: int):
+    l = [(i, i + 1) for i in range(size)]
+    for i in range(calls):
+        results = await async_collection_add(l)
+    return
+
+
+async def async_python(calls: int, size: int):
+    l = [(i, i + 1) for i in range(size)]
+    for i in range(calls):
+        results = await async_collection_add_python(l)
+    return
+
+
+async def tokio_rust(calls: int, size: int):
+    l = [(i, i + 1) for i in range(size)]
+    for i in range(calls):
+        results = await async_collection_tokio_add(l)
+    return
+
+
+async def tokio_python(calls: int, size: int):
+    l = [(i, i + 1) for i in range(size)]
+    for i in range(calls):
+        results = await async_collection_tokio_add_python(l)
+    return
+
+
+async def measure(calls: int, size: int, rust_function, python_function, type: str):
+    start = time.perf_counter()
+    _ = await rust_function(calls, size)
+    rust_time = time.perf_counter() - start
+
+    print(
+        f"{type} Rust with {calls} calls and list size {size} took {rust_time:.6f} seconds"
+    )
+
+    start = time.perf_counter()
+    _ = await python_function(calls, size)
+    python_time = time.perf_counter() - start
+
+    print(
+        f"{type} Python with {calls} calls and list size {size} took {python_time:.6f} seconds"
+    )
+
+    return {
+        "calls": calls,
+        "size": size,
+        "rust_time": rust_time,
+        "python_time": python_time,
+        "type": type,
+    }
+
+async def main():
+    print("py03-benchmark")
+
+    call_size_list_async = [
+        (4000000, 1),
+        (2500, 10000),
+        (10, 2000000),
+    ]
+
+    results = []
+
+    for calls, size in call_size_list_async:
+        result = await measure(calls, size, async_rust, async_python, "Async")
+        results.append(result)
+
+    call_size_list_tokio = [
+        (1500000, 1),
+        (2500, 3000),
+        (5, 2000000),
+    ]
+
+    for calls, size in call_size_list_tokio:
+        result = await measure(calls, size, tokio_rust, tokio_python, "Tokio")
+        results.append(result)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- python/py03_benchmark/py03_benchmark.py
+++ python/py03_benchmark/py03_benchmark.py
@@ -1,1 +1,5 @@
-from ._rust.py03_benchmark import sum_as_string
+from ._rust.py03_benchmark import (
+    async_collection_add,
+    async_collection_tokio_add,
+    sum_as_string,
+)
